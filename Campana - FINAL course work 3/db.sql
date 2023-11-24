057e8837ec0c02a2b372c1432a6d325835a5e718
apollo09460 <151806019+apollo09460@users.noreply.github.com>
2023-11-24 12:33:37 +0800

    db.sql

--- Campana - FINAL course work 3/db.sql
+++ Campana - FINAL course work 3/db.sql
@@ -1,6 +1,14 @@
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 create database if not exists tms;
 
 use tms;
+
+
 
 CREATE TABLE `tasks` (
   `taskId` int(11) NOT NULL,
@@ -9,64 +17,21 @@ CREATE TABLE `tasks` (
   `username` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `tasks`
---
 
-INSERT INTO `tasks` (`taskId`, `taskName`, `is_done`, `username`) VALUES
-(194, 'cook', 1, 'von'),
-(195, '', 1, 'mike'),
-(196, '', 1, 'von'),
-(197, '', 1, 'mike'),
-(198, '', 1, 'mike');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `username` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `users`
---
 
-INSERT INTO `users` (`username`) VALUES
-('bogart'),
-('mike'),
-('von');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tasks`
---
 ALTER TABLE `tasks`
   ADD PRIMARY KEY (`taskId`),
   ADD KEY `test` (`username`);
 
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`username`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `tasks`
---
 ALTER TABLE `tasks`
   MODIFY `taskId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=199;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
